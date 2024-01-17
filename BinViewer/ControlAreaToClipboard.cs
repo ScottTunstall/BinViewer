4c56b1c339a2d7bab18915f470987da2c9f3f131
Scott Tunstall <scott.tunstall@ntlworld.com>
2024-01-17 15:42:44 +0000

    Created the ControlAreaToClipboard class which, as the name implies, takes an area of a Winforms Control and copies it to the clipboard as a bitmap

---
+++ BinViewer/ControlAreaToClipboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinViewer
+{
+    public class ControlAreaToClipboard
+    {
+        public void CopyFrom(Control source, Point location, int width, int height)
+        {
+            using Image img = new Bitmap(width, height);
+            using var g = Graphics.FromImage(img);
+            g.CopyFromScreen(source.PointToScreen(location), new Point(0, 0), new Size(width, height));
+            Clipboard.SetImage(img);
+        }
+    }
+}
